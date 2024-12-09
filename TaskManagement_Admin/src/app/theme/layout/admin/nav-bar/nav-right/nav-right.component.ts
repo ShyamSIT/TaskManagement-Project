6555ef3ff2d16610dbf944ed32854b5c74ea6736
ShyamSIT <shyam.d@shaligraminfotech.com>
2024-12-09 14:48:29 +0530

    fixed  : navbar component

--- TaskManagement_Admin/src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
+++ TaskManagement_Admin/src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
@@ -1,9 +1,28 @@
 // Angular import
 import { Component } from '@angular/core';
+import { ApiUrlHelper } from 'src/app/config/apiUrlHelper';
+import { CommonService } from 'src/app/core/services/common.service';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-nav-right',
   templateUrl: './nav-right.component.html',
   styleUrls: ['./nav-right.component.scss']
 })
-export class NavRightComponent {}
+export class NavRightComponent {
+  
+  constructor(
+    private apiUrl: ApiUrlHelper,
+    private commonService: CommonService,
+    private storageService: StorageService
+  ) {}
+
+  public fullname : string = ''
+
+  ngOnInit(): void {
+    const FirstName = this.storageService.getValue('FirstName');
+    const LastName = this.storageService.getValue('LastName');
+
+    this.fullname = FirstName + ' ' + LastName;
+  }
+}
